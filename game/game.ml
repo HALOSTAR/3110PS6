c12c05f7935468e929a77920b8de2943ce2d15fe
Lydia Wang <lw354@cornell.edu>
2012-05-03 03:42:25 -0400

    handleTime not written

--- game/game.ml
+++ game/game.ml
@@ -3,17 +3,17 @@ open Constants
 open Util
 open State
 
-type game = state ref * int ref * Mutex.t (*No, This needs to be changed based
+type game = state ref * float ref * Mutex.t (*No, This needs to be changed based
  on your implementation*)
 
-let initGame () : game =
+(*let initGame () : game =
   let st = ref (empty ()) in
   let s = ref 0 in
   let m = Mutex.create () in
   Netgraphics.send_update(InitGraphics);
   Netgraphics.send_update(InitFood(cFOOD_TILES));
   Netgraphics.send_update(InitWood(cWOOD_TILES));
-  (st, s, m)
+  (st, s, m)*)
 
 (* game -> unit. Places intial units and buildings using pixels *)
 let initUnitsAndBuildings ((st, s, m):game) : unit =
@@ -60,23 +60,57 @@ let handleAction g act c : command =
      * colors are not equal. Else, match against all the possible actions.
      *)
     match act with
-      | QueueCollect unit_id ->
+     (* | QueueCollect unit_id ->
           let pos = checkid_checkunit !st unit_id c in
           let (boolean, state) = update_score_resource !st pos c in
           if (pos <> (-1., -1.) && boolean)
-          then ((st := state); (Netgraphics.add_update(DisplayString
-(c, "successful"))); 
+          then ((st := state); 
 Success) else 
-(Netgraphics.add_update(DisplayString(c, "failed")); Failed)
-      | QueueMove(unit_id,pos) -> failwith "not implemented"
+(Netgraphics.add_update(DisplayString(c, "failed")); Failed)*)
+      | QueueMove(unit_id,pos) -> if (check_unit_id !st c unit_id = false) || 
+((is_valid_pos pos) = false) then Failed 
+        else (st := (queue_moves !st c unit_id 
+(get_moves (!st) (unit_id) (c) (pos))); Success) 
+            
     | Talk str -> Netgraphics.add_update(DisplayString(c, str)); Success
-		| QueueAttack (unit_id, attackable_object) -> failwith "not implemented"
-		| QueueBuild (unit_id, building_type) -> failwith "not implemented"
-		| QueueSpawn (building_id, unit_type) -> failwith "not implemented"
-		| ClearAttack id -> failwith "not implemented" 
-		| ClearMove id -> failwith "not implemented"
-		| Upgrade upgrade_type -> failwith "not implemented"
-		in
+    | QueueAttack (unit_id, attackable_object) ->
+        let target_id =
+          match attackable_object with
+            Building b -> b
+            | Unit u -> u in
+        if ((get_unit_type !st c unit_id) = Villager) || 
+           ((check_unit_id !st c unit_id )= false) || 
+           (check_unit_id !st c target_id) ||
+           ((get_unit_health !st c unit_id) = 0) 
+        then Failed
+        else (st := (queue_attack !st c unit_id attackable_object); Success)
+
+    | QueueBuild (unit_id, building_type) -> 
+        if (building_type <> Barracks) ||
+           ((check_unit_id !st c unit_id) = false) ||
+           ((get_unit_type !st c unit_id) <> Villager) ||
+           ((check_tiles_valid_empty !st c unit_id) = false) ||
+           ((check_enough_resources !st c building_type) = false) then Failed
+        else (st := (start_build !st c unit_id); Success)
+
+    | QueueSpawn (building_id, unit_type) -> 
+        if ((check_unit_id !st c building_id) = false) ||
+           ((check_enough_resources !st c unit_type) = false) ||
+           ((unit_matches_upgrade !st c unit_type) = false) ||
+           ((unit_matches_age !st c unit_type) = false) then Failed
+        else (spawn_unit !st c building_id unit_type; Success)
+
+    | ClearAttack id ->
+        if ((check_unit_id !st c id) = false) then Failed
+        else (clear_attack !st c id; Success)
+
+    | ClearMove id ->
+        if ((check_unit_id !st c id) = false) then Failed
+        else (clear_move !st c id; Success)
+
+    | Upgrade upgrade_type ->
+        if upgrade_types !st c upgrade_type then Success else Failed
+  in
   Mutex.unlock m;
   Result res
 
@@ -85,11 +119,12 @@ let handleStatus g status : command =
   Mutex.lock m;
   let data =
     match status with
-			| TeamStatus c -> failwith "not implemented"
-			| UnitStatus id -> failwith "not implemented"
-			| BuildingStatus id -> failwith "not implemented"
-			| GameStatus -> failwith "not implemented"
-			| ResourceStatus -> failwith "not implemented"
+      | TeamStatus c -> TeamData (get_team_status !st c)
+      | UnitStatus id -> UnitData (get_unit_status !st id)
+      | BuildingStatus id -> BuildingData (get_building_status !st id)
+      | GameStatus -> GameData ((get_team_status !st Red), 
+          (get_team_status !st Blue), !s)
+      | ResourceStatus -> ResourceData (get_resource_status !st)
     in
   Mutex.unlock m;
   Data data
